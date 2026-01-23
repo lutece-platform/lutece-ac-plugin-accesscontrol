33490a78637e090adf9d525e4c0acee70534dc32
hohll <laurent.hohl@gmail.com>
2026-01-23 16:13:06 +0100

    LUT-31733 : Udpare template to V8

--- src/sql/plugins/accesscontrol/core/init_core_accesscontrol.sql
+++ src/sql/plugins/accesscontrol/core/init_core_accesscontrol.sql
@@ -7,11 +7,11 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'ACCESSCONTROL_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('ACCESSCONTROL_MANAGEMENT','accesscontrol.adminFeature.ManageAccessControl.name',1,'jsp/admin/plugins/accesscontrol/ManageAccessControls.jsp','accesscontrol.adminFeature.ManageAccessControl.description',0,'accesscontrol',NULL,NULL,NULL,4);
+('ACCESSCONTROL_MANAGEMENT','accesscontrol.adminFeature.ManageAccessControl.name',1,'jsp/admin/plugins/accesscontrol/ManageAccessControls.jsp','accesscontrol.adminFeature.ManageAccessControl.description',0,'accesscontrol',NULL,'ti ti-lock-access',NULL,4);
 
 DELETE FROM core_admin_right WHERE id_right = 'USERCODES_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('USERCODES_MANAGEMENT','accesscontrol.adminFeature.ManageUserCodes.name',1,'jsp/admin/plugins/accesscontrol/ManageControllerUserCode.jsp','accesscontrol.adminFeature.ManageUserCodes.description',0,'accesscontrol',NULL,NULL,NULL,4);
+('USERCODES_MANAGEMENT','accesscontrol.adminFeature.ManageUserCodes.name',1,'jsp/admin/plugins/accesscontrol/ManageControllerUserCode.jsp','accesscontrol.adminFeature.ManageUserCodes.description',0,'accesscontrol',NULL,'ti ti-user-scan',NULL,4);
 
 
 --
